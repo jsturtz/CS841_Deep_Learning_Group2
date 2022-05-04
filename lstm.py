f3cbd317fdf815d57c96f462156135c156dd5364
Jordan <jabestur@gmail.com>
2022-05-04 21:07:01 +0100

    Fixed the printing, added support for predicting both directions

--- lstm.py
+++ lstm.py
@@ -1,6 +1,9 @@
 
 import functools
 import json
+from this import d
+from turtle import forward
+from unicodedata import bidirectional
 import numpy as np
 import os
 import time
@@ -8,12 +11,20 @@ from datetime import datetime
 import sys
 
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Conv1D, Bidirectional, Embedding, MaxPooling1D
+
+# TESTING FUNCTIONAL API
+# from keras.layers import Dense, LSTM, Conv1D, Bidirectional, Embedding, MaxPooling1D, Input
+from tensorflow import keras
+from tensorflow.keras import layers
+
 from keras.utils import np_utils
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from pygments import highlight
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import Callback
+
+from colors import bcolors
 
 # Use monospace to make formatting cleaner
 plt.rcParams.update({'font.family':'monospace'})
@@ -184,9 +195,15 @@ def generate_input_output_pairs(sequence, kmer_length):
     # Extract all input_output pairs from all sequences
     input_output_pairs = []
     for seq in sequence:
-        for index in range(kmer_length, len(seq)):
-            seq_in = seq[index - kmer_length:index]
-            seq_out = seq[index]
+        for start in range(len(seq)-kmer_length):
+            end = start + kmer_length
+            seq_in = seq[start:end]
+            seq_out = seq[end]
+            input_output_pairs.append((seq_in, seq_out))
+
+            # FIXME: Testing whether it's better to add reversed data?
+            seq_in = seq[end:start:-1]
+            seq_out = seq[start]
             input_output_pairs.append((seq_in, seq_out))
 
     return input_output_pairs
@@ -242,15 +259,64 @@ def predict_gaps(seq, model, kmer_length, char_to_int_mapping, int_to_char_mappi
     return functools.reduce(lambda a, b: a+b, predicted_seq)
 
 # =============================================================================
-def print_sequence(seq, header=""):
+def predict_gaps_both_directions(seq, model, kmer_length, char_to_int_mapping, int_to_char_mapping, gap_char="-"):
+
+    forward_seq = predict_gaps(seq, model, kmer_length, char_to_int_mapping, int_to_char_mapping, gap_char)
+    reverse_seq = predict_gaps(seq[::-1], model, kmer_length, char_to_int_mapping, int_to_char_mapping, gap_char)
+
+    full_seq = []
+    for forward_pred, reverse_pred in zip(forward_seq, reverse_seq[::-1]):
+        # FIXME: I guess just default to the forward prediction unless missing? IDK
+        if forward_pred == gap_char:
+            full_seq.append(reverse_pred)
+        else:
+            full_seq.append(forward_pred)
+
+    # Convert back to a single string when finished
+    return functools.reduce(lambda a, b: a+b, full_seq)
+
+# =============================================================================
+def get_nonmatching_indices(seq1, seq2):
+    s = set()
+    for i, (c1, c2) in enumerate(zip(seq1, seq2)):
+        if c1 != c2:
+            s.add(i)
+    return s
+
+# =============================================================================
+def highlight_indices(seq, indices, color):
+    seq = list(seq)
+    for i, char in enumerate(seq):
+        if i in indices:
+            seq[i] = f"{color}{char}{bcolors.ENDC}"
+
+    return functools.reduce(lambda a, b: a+b, seq)
+
+# =============================================================================
+def print_sequence(seq, header=None, indices_to_highlight=None):
+
+    line_length = 40
+    group_length = 10
+
     if header:
         print(header)
     print("=====================================================================")
-    line_length = 50
-    group_length = 10
+
     lines = [seq[begin:begin+line_length] for begin in range(0, len(seq), line_length)]
-    for line in lines:
+    for line_num, line in enumerate(lines):
+
         groups = [line[begin:begin+group_length] for begin in range(0, len(line), group_length)]
+        if indices_to_highlight:
+
+            # FIXME: I hate this shit so much. Figure out a more elegant solution
+            for group_num, group in enumerate(groups):
+                start_group_index = line_num * line_length + group_num * group_length
+                group_indices = range(start_group_index, start_group_index + group_length)
+                missing_group_indices = indices_to_highlight.intersection(group_indices)
+                missing_group_indices = set([item - start_group_index for item in missing_group_indices])
+
+                groups[group_num] = highlight_indices(group, missing_group_indices, bcolors.FAIL)
+
         print("\t".join(groups))
 
 # =============================================================================
@@ -263,10 +329,26 @@ def get_sequences(fasta_file):
     return sequences
 
 # =============================================================================
+def compute_accuracy(seq1, seq2, ignore_first=None):
+    '''
+    Very simple way of measuring accuracy according to matching indices
+    Assumes seq1 and seq2 have the same length
+    ignore_first optional argument to ignore comparing first N characters
+    '''
+    if len(seq1) != len(seq2):
+        raise Exception("Dumbass, they need to be the same length!")
+
+    if ignore_first:
+        seq1 = seq1[ignore_first:]
+        seq2 = seq2[ignore_first:]
+    return sum(1 if c1 == c2 else 0 for c1, c2 in zip(seq1, seq2)) / len(seq1)
+
+# =============================================================================
 def main():
 
-    training_sequences = get_sequences("training_sequences.txt")
+    training_sequences = get_sequences("training_sequences_10.txt")
     target_sequence = get_sequences("target_sequence.txt")[0]
+    target_sequence_reversed = target_sequence[::-1]
 
     # extract all chars from all sequences to create our mappings and to determine classes
     all_chars = set("".join(training_sequences) + target_sequence)
@@ -279,6 +361,7 @@ def main():
 
     training_pairs = generate_input_output_pairs(training_sequences, KMER_LENGTH)
     testing_pairs = generate_input_output_pairs([target_sequence], KMER_LENGTH)
+    testing_pairs_reversed = generate_input_output_pairs([target_sequence_reversed], KMER_LENGTH)
 
     # Shuffle the training data so no bias is introduced when splitting for validation
     np.random.shuffle(training_pairs)
@@ -290,15 +373,36 @@ def main():
     trainX, trainY = preprocess_data(training_pairs[validation_threshold:], char_to_int)
     validX, validY = preprocess_data(training_pairs[:validation_threshold], char_to_int)
     testX, testY = preprocess_data(testing_pairs, char_to_int)
+    testX_rev, testY_rev = preprocess_data(testing_pairs_reversed, char_to_int)
 
+    print(trainX.shape)
+    print(trainY.shape)
     # create the model
-    model = Sequential()
-    model.add(Conv1D(FIRST_CONV_FILTERS, FIRST_CONV_KERNEL_SIZE))
-    # model.add(Bidirectional(LSTM(NUM_LSTM_LAYERS), input_shape=(trainX.shape[1], trainX.shape[2])))
-    model.add(LSTM(NUM_LSTM_LAYERS, input_shape=(trainX.shape[1], trainX.shape[2])))
-    model.add(Dense(FIRST_DENSE_LAYER, activation=HIDDEN_LAYER_ACTIVATION_FUNC))
-    model.add(Dense(SECOND_DENSE_LAYER, activation=HIDDEN_LAYER_ACTIVATION_FUNC))
-    model.add(Dense(NUM_CLASSES, activation=OUTPUT_ACTIVATION_FUNC))
+    # model = Sequential()
+    # model.add(Conv1D(FIRST_CONV_FILTERS, FIRST_CONV_KERNEL_SIZE))
+    # model.add(Bidirectional(LSTM(NUM_LSTM_LAYERS, return_sequences=True), input_shape=(trainX.shape[1], trainX.shape[2])))
+    # # model.add(LSTM(NUM_LSTM_LAYERS, input_shape=(trainX.shape[1], trainX.shape[2])))
+    # model.add(Dense(FIRST_DENSE_LAYER, activation=HIDDEN_LAYER_ACTIVATION_FUNC))
+    # model.add(Dense(SECOND_DENSE_LAYER, activation=HIDDEN_LAYER_ACTIVATION_FUNC))
+    # model.add(Dense(NUM_CLASSES, activation=OUTPUT_ACTIVATION_FUNC))
+    # model.compile(loss=COST_FUNC, optimizer=OPTIMIZER, metrics=['accuracy'])
+
+    # FIXME: Is the right way to train on just the forward data, or to create the
+    # reverse data and train on that? If I don't create the reverse data, the bidirectional
+    # does not perform well predicting reversed sequences. But if I do train on the reversed
+    # Data, the accuracy is high regardless of what tweaks I make to the model.
+    # I.e. the bidirectional does not seem to help much
+
+    inputs = keras.Input(shape=(KMER_LENGTH, 1))
+    outputs = layers.LSTM(NUM_LSTM_LAYERS)(inputs)
+    # outputs = layers.Conv1D(FIRST_CONV_FILTERS, FIRST_CONV_KERNEL_SIZE)(inputs)
+    # outputs = layers.LSTM(NUM_LSTM_LAYERS)(outputs)
+    # outputs = layers.Bidirectional(layers.LSTM(NUM_LSTM_LAYERS, return_sequences=True))(outputs)
+    # outputs = layers.Bidirectional(layers.LSTM(NUM_LSTM_LAYERS))(outputs)
+    # outputs = layers.Dense(FIRST_DENSE_LAYER, activation=HIDDEN_LAYER_ACTIVATION_FUNC)(outputs)
+    # outputs = layers.Dense(SECOND_DENSE_LAYER, activation=HIDDEN_LAYER_ACTIVATION_FUNC)(outputs)
+    outputs = layers.Dense(NUM_CLASSES, activation=OUTPUT_ACTIVATION_FUNC)(outputs)
+    model = keras.Model(inputs=inputs, outputs=outputs)
     model.compile(loss=COST_FUNC, optimizer=OPTIMIZER, metrics=['accuracy'])
 
     # fit the data, summarize performance of the model
@@ -318,25 +422,59 @@ def main():
 
     model.summary()
     _, accuracy = model.evaluate(testX, testY, verbose=0)
-    print(f"accuracy: {accuracy:.2f}")
-    save_result(f"{accuracy:.2f}", history.history, model, secs_to_train)
+    print(f"accuracy: {accuracy:.4f}")
+    # save_result(f"{accuracy:.2f}", history.history, model, secs_to_train)
+
+    _, accuracy = model.evaluate(testX_rev, testY_rev, verbose=0)
+    print(f"accuracy in reverse direction: {accuracy:.4f}")
 
     # For funsies, use the model to predict the gaps in the de novo sequence
     # Make sure target sequence is not in training data
     missing_indices = set([0, 1, 2, 24, 25, 26, 62, 63, 64, 66, 67, 68, 69, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 209, 210, 211, 212, 213])
 
-    # FIXME: We can't predict in the reverse direction for now
-    missing_indices = set(item for item in missing_indices if item > KMER_LENGTH)
-    de_novo_sequence = "".join(c if i not in missing_indices else "-" for i, c in enumerate(target_sequence))
+    # The reversed string has missing indices which are the complement of the forward direction
+    missing_indices_reversed = set([len(target_sequence) - 1 - i for i in missing_indices])
 
-    predicted_sequence = predict_gaps(de_novo_sequence, model, KMER_LENGTH, char_to_int, int_to_char, gap_char="-")
+    de_novo_sequence = "".join(c if i not in missing_indices else "-" for i, c in enumerate(target_sequence))
+    de_novo_sequence_reversed = de_novo_sequence[::-1]
+
+    print(missing_indices)
+    print(missing_indices_reversed)
+
+    pred_sequence_forward = predict_gaps(de_novo_sequence, model, KMER_LENGTH, char_to_int, int_to_char, gap_char="-")
+    pred_sequence_reverse = predict_gaps(de_novo_sequence_reversed, model, KMER_LENGTH, char_to_int, int_to_char, gap_char="-")
+    pred_sequence_full = predict_gaps_both_directions(de_novo_sequence, model, KMER_LENGTH, char_to_int, int_to_char, gap_char="-")
+
+    forward_incorrect = get_nonmatching_indices(target_sequence, pred_sequence_forward)
+    reverse_incorrect = get_nonmatching_indices(target_sequence_reversed, pred_sequence_reverse)
+    full_incorrect = get_nonmatching_indices(target_sequence, pred_sequence_full)
+
+    print("Forward Incorrect: {}".format(forward_incorrect))
+    print("Reverse Incorrect: {}".format(reverse_incorrect))
+    print("Full Incorrect: {}".format(full_incorrect))
 
     print_sequence(target_sequence, "TARGET SEQUENCE")
-    print_sequence(predicted_sequence, "PREDICTED SEQUENCE")
+    print_sequence(de_novo_sequence,"DE NOVO SEQUENCE", missing_indices)
+    print_sequence(pred_sequence_full, "PREDICTED SEQUENCE FULL", full_incorrect)
+    print_sequence(pred_sequence_forward, "PREDICTED SEQUENCE FORWARD", forward_incorrect)
 
-    num_correct_gaps = sum(1 if c1 == c2 and index in missing_indices else 0 for index, (c1, c2) in enumerate(zip(predicted_sequence, target_sequence)))
-    gaps_to_predict = len(missing_indices)
-    print(f"Accuracy on De Novo Sequence: {num_correct_gaps / gaps_to_predict}")
+    print_sequence(target_sequence_reversed, "TARGET SEQUENCE REVERSE")
+    print_sequence(de_novo_sequence_reversed,"DE NOVO SEQUENCE REVERSE", missing_indices_reversed)
+    print_sequence(pred_sequence_reverse, "PREDICTED SEQUENCE REVERSE", reverse_incorrect)
+
+    # FIXME: Now that I'm taking the effor to tget the nonmatching indices, just use that for accuracy
+    # forward_accuracy = compute_accuracy(pred_sequence_forward, target_sequence, ignore_first=KMER_LENGTH)
+    # reverse_accuracy = compute_accuracy(pred_sequence_reverse, target_sequence[::-1], ignore_first=KMER_LENGTH)
+    # full_accuracy = compute_accuracy(target_sequence, pred_sequence_full)
+
+    target_len = len(target_sequence)
+    forward_accuracy = (target_len - len(forward_incorrect)) / target_len
+    reverse_accuracy = (target_len - len(reverse_incorrect)) / target_len
+    full_accuracy = (target_len - len(full_incorrect)) / target_len
+
+    print(f"Accuracy on De Novo Sequence in forward direction: {forward_accuracy}")
+    print(f"Accuracy on De Novo Sequence in reverse direction: {reverse_accuracy}")
+    print(f"Accuracy on De Novo Sequence when combining both: {full_accuracy}")
 
 if __name__ == "__main__":
     main()
